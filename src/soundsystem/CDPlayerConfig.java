f7770cbf83aab4f8f41b8aba028e889a48ef1604
MadMagezz <yan6ex@yandex.ru>
2018-05-06 22:08:20 +0300

    adding project to git. Test, configuration

---
+++ src/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,23 @@
+package soundsystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"soundsystem", "video"})
+public class CDPlayerConfig {
+
+    @Bean(name="lonelyHeartsClubBand")
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CDPlayer cdPlayer() {
+        return new CDPlayer(sgtPeppers());
+    }
+
+
+
+}
